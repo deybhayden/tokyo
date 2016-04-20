436c625e9b05bf39ed69f81ae7e89575c78bee16
Ben Hayden <hayden767@gmail.com>
2016-04-20 17:01:18 -0500

    More google docs

--- config.py
+++ config.py
@@ -3,10 +3,24 @@ import logging
 
 LOG_LEVEL = logging.WARNING
 PLATFORM = "text"
+
+# == SLACK ==
 SLACK_TOKEN = "yourtoken"
 # Find the bot's Slack ID. You can obtain this using Slack's API tester - https://api.slack.com/methods/users.list
 # or by inspecting the tokyo debug logs (by setting LOG_LEVEL = logging.DEBUG). It should look like U023BECGF.
 SLACK_USER_ID = "U12345678"
+
+# === GOOGLE ===
+#
+# You'll need to create a google project from the Google Developer Console and enable the following APIs:
+# Admin SDK, Gmail API
+# You then need to create a service account with domain delegation authority.
+# NOTE: Make sure that the service account's client
+# ID has been authorized to act with domain authority on the following scopes:
+# https://www.googleapis.com/auth/admin.directory.domain.readonly
+# https://www.googleapis.com/auth/admin.directory.user
+# https://www.googleapis.com/auth/admin.directory.group
+# https://www.googleapis.com/auth/gmail.send
 # This should contain your Google Service Account key json file contents (downloaded when creating a google service account from the google developer console).
 # Paste the contents of the file here as a python dictionary.
 GOOGLE_SERVICE_ACCOUNT_JSON = {}
