9cf96fffd7f6a7508fbdd773c9be0832419aa8a0
Ben Hayden <hayden767@gmail.com>
2016-04-21 13:58:54 -0500

    Close websocket on ctl+c

--- platforms/slack.py
+++ platforms/slack.py
@@ -13,29 +13,33 @@ def main(config):
 
     sc = SlackClient(config.SLACK_TOKEN)
 
-    if sc.rtm_connect():
-        logging.info("Listening for incoming messages...")
+    try:
+        if sc.rtm_connect():
+            logging.info("Listening for incoming messages...")
 
-        while True:
-            events = sc.rtm_read()
-            for event in events:
-                response_required = all([event['type'] == 'message',
-                                         event.get('text'),
-                                         event.get('user') != config.SLACK_USER])
+            while True:
+                events = sc.rtm_read()
+                for event in events:
+                    response_required = all([event['type'] == 'message',
+                                             event.get('text'),
+                                             event.get('user') != config.SLACK_USER])
 
-                if response_required:
-                    logging.debug(event)
-                    responses = gz_chat.respond(event['text'], context=event)
-                    try:
-                        for response in responses:
+                    if response_required:
+                        logging.debug(event)
+                        responses = gz_chat.respond(event['text'], context=event)
+                        try:
+                            for response in responses:
+                                sc.api_call(
+                                    "chat.postMessage", channel=event['channel'], as_user=True,
+                                    text=response
+                                )
+                        except:
+                            logging.exception("Error generated responding to < {} >.".format(event['text']))
                             sc.api_call(
                                 "chat.postMessage", channel=event['channel'], as_user=True,
-                                text=response
+                                text="An error occurred - check the logs."
                             )
-                    except:
-                        logging.exception("Error generated responding to < {} >.".format(event['text']))
-                        sc.api_call(
-                            "chat.postMessage", channel=event['channel'], as_user=True,
-                            text="An error occurred - check the logs."
-                        )
-            time.sleep(1)
+                time.sleep(1)
+    except (KeyboardInterrupt):
+        sc.server.websocket.close()
+        logging.info("Exiting...")
