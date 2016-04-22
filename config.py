3585ba413b2163f6b107c6be9d9a47cf3f801879
Ben Hayden <hayden767@gmail.com>
2016-04-22 15:51:36 -0500

    Add Trello config

--- config.py
+++ config.py
@@ -4,14 +4,13 @@ import logging
 LOG_LEVEL = logging.WARNING
 PLATFORM = "text"
 
-# == SLACK ==
+# === SLACK ===
 SLACK_TOKEN = "yourtoken"
 # Find the bot's Slack ID. You can obtain this using Slack's API tester - https://api.slack.com/methods/users.list
 # or by inspecting the tokyo debug logs (by setting LOG_LEVEL = logging.DEBUG). It should look like U023BECGF.
 SLACK_USER_ID = "U12345678"
 
 # === GOOGLE ===
-#
 # You'll need to create a google project from the Google Developer Console and enable the following APIs:
 # Admin SDK, Gmail API
 # You then need to create a service account with domain delegation authority.
@@ -34,6 +33,14 @@ GOOGLE_GROUPS = {
     'GDES': ['design'],
     'GDEV': ['dev']
 }
+
+# === TRELLO ===
+# Org ID or name
+TRELLO_ORG = 'yourorg'
+# Get this key from https://trello.com/app-key
+TRELLO_API_KEY = 'yourkey'
+# Generate a token via an API call - http://stackoverflow.com/questions/17178907/how-to-get-a-permanent-user-token-for-writes-using-the-trello-api
+TRELLO_TOKEN = 'yourtoken'
 
 if os.path.exists('config_private.py'):
     # Use config_private for your own personal settings - default to be git ignored.
