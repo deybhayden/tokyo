aeb69dbf17f921bc7af1bf4ec50bd93dad542b98
Ben Hayden <hayden767@gmail.com>
2016-05-02 17:02:59 -0500

    More coverage

--- tests/test_tokyo.py
+++ tests/test_tokyo.py
@@ -11,12 +11,19 @@ import os
 import runpy
 import sys
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, call
 
 TESTS_DIR = os.path.dirname(__file__)
 sys.path.append(TESTS_DIR)
 ROOT_DIR = os.path.join(os.path.dirname(__file__), os.pardir)
 sys.path.append(ROOT_DIR)
+
+def resp_generator(responses):
+    for response in responses:
+        if isinstance(response, BaseException):
+            raise response
+        else:
+            yield response
 
 
 class TestText(unittest.TestCase):
@@ -26,35 +33,56 @@ class TestText(unittest.TestCase):
 
         Mock godzillops.Chat class and ovrride config.py loaded by tokyo's main.py
         """
+        # First, we need to create our per-test mocks
+        # == Override existing config with test config ==
         import config_test
         self.config = config_test
-        # First, we need to create our per-test mocks
-
-        # == Override existing config with test config ==
-        self.config_patch = patch.dict('sys.modules', {'config': self.config})
-        self.config_patch.start()
-
-        # == Godzillops Chat Class ==
-        self.gz_chat_mock = Mock(name='godzillops.Chat')
-        self.gz_chat_responses = ()
-        self.gz_chat_mock().respond = Mock(return_value=self.gz_chat_responses)
-        self.gz_patch = patch('godzillops.Chat', self.gz_chat_mock)
-        self.gz_patch.start()
+        self.logging_mock = Mock(name='logging')
+        self.modules_patch = patch.dict('sys.modules', {'config': self.config, 'logging': self.logging_mock})
+        self.modules_patch.start()
 
     def tearDown(self):
-        self.gz_patch.stop()
-        self.config_patch.stop()
+        self.modules_patch.stop()
 
     def test_000_text_chat_successful(self):
         """Make sure we initialized a text Chat platform without exception."""
         self.config.PLATFORM = "text"
-        mocks = {
+
+        # Test specific mocks
+        gz_chat_mock = Mock(name='godzillops.Chat')
+        gz_chat_mock().respond = Mock(return_value=resp_generator(('Huh?',)))
+        _builtins = {
             'print': Mock(),
             'input': Mock(side_effect=['Hey', 'Bye', EOFError('FIN')])
         }
-        with patch.multiple('platforms.text', **mocks):
-            runpy.run_module('main', run_name='__main__')
-            mocks['print'].assert_called_with('Exiting...')
+
+        with patch('godzillops.Chat', gz_chat_mock):
+            with patch.multiple('builtins', **_builtins):
+                runpy.run_module('main', run_name='__main__')
+
+        gz_chat_mock().respond.assert_has_calls((call('Hey'), call('Bye')))
+        _builtins['print'].assert_has_calls((call('Huh?'), call('Exiting...')))
+
+    def test_001_text_chat_respond_exception(self):
+        """Force an exception on respond in text Chat platform."""
+        self.config.PLATFORM = "text"
+
+        # Test specific mocks
+        gz_chat_mock = Mock(name='godzillops.Chat')
+        gz_chat_mock().respond = Mock(side_effect=[resp_generator(('Huh?',)), resp_generator((ValueError('BOOM'),))])
+        _builtins = {
+            'print': Mock(),
+            'input': Mock(side_effect=['Hey', 'Bye', EOFError('FIN')])
+        }
+
+        with patch('godzillops.Chat', gz_chat_mock):
+            with patch.multiple('builtins', **_builtins):
+                runpy.run_module('main', run_name='__main__')
+
+        gz_chat_mock().respond.assert_has_calls((call('Hey'), call('Bye')))
+        _builtins['print'].assert_has_calls((call('Huh?'), call('An error occurred - check the logs.'), call('Exiting...')))
+        self.logging_mock.exception.assert_called_with("Error generated responding to < Bye >.")
+        self.assertTrue(gz_chat_mock().cancel.called)
 
 
 if __name__ == '__main__':
